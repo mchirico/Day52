e3d6d766cbbf06d3c70272f6559f858b31caca37
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-12-05 11:40:31 -0500

    Initial Commit

---
+++ Day52/Day52App.swift
@@ -0,0 +1,17 @@
+//
+//  Day52App.swift
+//  Day52
+//
+//  Created by Mike Chirico on 12/5/20.
+//
+
+import SwiftUI
+
+@main
+struct Day52App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
