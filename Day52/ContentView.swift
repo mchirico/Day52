3009975069687d4f6341ea7cb76bd281e15d45f2
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-12-05 12:22:20 -0500

    update-automated-save

--- Day52/ContentView.swift
+++ Day52/ContentView.swift
@@ -9,8 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         Text("Hello, world!")
             .padding()
+        
+        
+        
+        Button(action: {
+                    
+                    let sb = SqliteBroker()
+                    sb.myStart()
+                    print("file url: \(sb.getDatabaseFileURL()) ")
+                    
+                    
+                    let textToShare = "SQLite Database"
+                    let url = sb.getDatabaseFileURL()
+                    let objectsToShare = [textToShare, url] as [Any]
+                    
+                    share(items: objectsToShare)
+                }){
+                    Text("SQLite Send")
+                        .padding(.all,30)
+                        .padding([.top,.bottom],20)
+                        .background(Color.blue)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(5)
+                        .shadow(radius: 25)
+                        .font(Font.custom("Avenir-Black", size: 27))
+                    
+                    
+                }
+        
+        
+        
     }
 }
 
